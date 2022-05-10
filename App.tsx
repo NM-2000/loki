23ab324ec9a3ccf1b9725ec84e80f11ce44e828c
NM-2000 <nikhilmittal3434@gmail.com>
2022-05-10 13:27:44 +0530

    TypeScript Added

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View style={{width: '100%', height: '100%', backgroundColor: 'tomato'}} ></View>
+  );
+};
+
+export default App;
